b38999e8a27d42b76f6007164a94e56eb6d0bc06
Siddhesh vilas sawant <100701921+siddhesh3607@users.noreply.github.com>
2022-12-02 22:55:18 +0530

    Create arrayjson.js

---
+++ arrayjson.js
@@ -0,0 +1,11 @@
+//array
+const a = ["Sids", 10, true, undefined]; // can contain anything
+
+// json  : key value pair
+const jsona = {
+  name: "Sids",
+  surname: "Sawant",
+  foodLinked: ["sweet", "pan"],
+};
+console.log(a);
+console.log(jsona.name);
