3349a80c43659328270eb36f1df315ff08bcd8a0
Siddhesh vilas sawant <100701921+siddhesh3607@users.noreply.github.com>
2022-12-02 22:57:55 +0530

    Create new.js

---
+++ new.js
@@ -0,0 +1,37 @@
+// console.log("solana");
+// console.log(typeof "ANnad ");
+// console.log(typeof null);
+// var a = "ANand";
+// let b = "is";
+// const c = "coding";
+// console.log(a + b + c);
+
+// js every error is undefined
+// try to keep everything const in js
+
+// const x = 25;
+// const y = 28;
+// if (undefined) {
+//   console.log("x is greater than y");
+// } else {
+//   console.log("y is greater");
+// }
+
+// for loop : inatilize => condition => execute => increment/decrement
+// repeation  ke liye use krte hey
+// const n = 10;
+
+// for (let i = 0; i < n; i++) {
+//   console.log(i);
+// }
+
+// error handling
+// try {
+//   //create error
+//   throw new Error("ANand is an error ");
+//   //   console.log(10 / 0);
+// } catch (err) {
+//   //catched error
+//   console.log(err);
+//   //   console.log("Anand is interesting");
+// }
